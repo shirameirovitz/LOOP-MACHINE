6e0052049eb818a3a92b2e0bf7a7be284103d223
Shira-Meirovitz <shirameirovi@gmail.com>
2021-12-21 16:11:30 +0200

    recorder and loop machine components

--- src/components/Pads.js
+++ src/components/Pads.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Pads.css';
 
 //all the mp3 sounds
 const Pads = ({ handlePadClick }) => {
---
+++ src/components/PlayPause.js
@@ -0,0 +1,29 @@
+/*
+Play-Pause buttons Component
+*/
+import React from 'react';
+import play from '../assets/play.png';
+import pause from '../assets/pause.png';
+
+const PlayPause = (props) => {
+  const { onClick } = props;
+  return (
+    <div className="btn-wrapper">
+      <img
+        className="wobble-hor-bottom"
+        id="playbtn"
+        src={play}
+        alt="playbtn"
+        onClick={onClick}
+      />
+      <img
+        className="wobble-hor-bottom"
+        id="pausebtn"
+        src={pause}
+        alt="pausebtn"
+        onClick={onClick}
+      />
+    </div>
+  );
+};
+export default PlayPause;
